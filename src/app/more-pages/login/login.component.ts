b5d3079cac947dd18ec82a4a6b3754be2ff4555d
orajiaku chibuchi <39632786+orajiakuchibuchi@users.noreply.github.com>
2021-12-11 10:06:21 +0100

    bug fixec

--- src/app/more-pages/login/login.component.ts
+++ src/app/more-pages/login/login.component.ts
@@ -14,9 +14,13 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
   isSubmitted  =  false;
-
+  // Add an object variable to show error and success messages'
+  message:any = {
+    error: null,
+    success: null
+  };
   constructor(private authenticationService: AuthenticationService, private router: Router, private formBuilder: FormBuilder ) {
-    
+
    }
 
   ngOnInit(): void {
@@ -24,18 +28,33 @@ export class LoginComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
   });
-    
+
   }
   get formControls() { return this.loginForm.controls; }
 
   login(){
     console.log(this.loginForm.value);
+    // Set them back to false
+    this.message.error = null;
+    this.message.success = null;
     this.isSubmitted = true;
     if(this.loginForm.invalid){
+      this.message.error = "Opps something went wrong. Ensure you fill in the right email format and a password up to 5 characters";
       return;
     }
-    this.authenticationService.login(this.loginForm.value);
-    this.router.navigateByUrl('/');
+    let form = new FormData();
+    form.append('email', this.loginForm.controls.email.value);
+    form.append('password', this.loginForm.controls.password.value);
+    // Get the returned value and assign it to the success key in the message variable
+    this.message.success = this.authenticationService.login(this.loginForm.value);
+    // Just setting a timer to make it  more realistic when things go over the network for delay
+    // Settimeout is a global function in javascrit that runs a specific function after a certain interval
+    setTimeout(()=>{
+      // I will alert for now, if you want it to redirect you to the home page,
+      // Comment the alert and Uncomment the return router line
+      // return alert(this.message.success);
+      return this.router.navigateByUrl('/');
+    }, 3000);
   }
 
   createNewlogin() {
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -15,7 +15,25 @@ export class AuthenticationService {
   }
 
   public login(userInfo: User){
-    localStorage.setItem('ACCESS_TOKEN', "access_token");
+    // When using Api you will be making the http call here which the calling function subscribes to.
+    const newToken = this.generateUniqueToken(32);
+    // I will console this when you click the submit button . when you check, go to your
+    console.log(newToken);
+    localStorage.setItem('ACCESS_TOKEN', newToken);
+    // application tab in your dev tool and locate the local storage menu.
+    // You should see a key of ACCESS_TOKEN with the value {newToken} which was consoled
+    // Return to the caller the string.
+    return 'You have sucessfully logged in';
+  }
+
+  // Generate random alphanumreic string between a specific range
+  private generateUniqueToken(length:number){
+    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var result = '';
+    for (var i = length; i > 0; --i){
+      result += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return result;
   }
 
   public isLoggedIn(){
